9be3ce5b7d4bb3eb86d4b529cfb7faf46630a21e
DavidIncarbone <david.incarbone@gmail.com>
2024-11-04 16:39:00 +0100

    done snack 6

--- Snack 6/script/script.js
+++ Snack 6/script/script.js
@@ -3,3 +3,26 @@ console.clear();
 
 /*Stampa il cubo dei primi N numeri,
 dove N è un numero indicato dall’utente.*/
+
+// Steps
+
+
+
+const totalNumbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const userNumbers = parseInt(prompt("Inserire i primi numeri da voler elevare al cubo"));
+console.log(`I numeri totali sono ${totalNumbers}. I primi ${userNumbers} sono da elevare al cubo.`);
+let cubeNumbers = [];
+cubeNumbers = totalNumbers.slice(0, userNumbers);
+console.log(cubeNumbers);
+
+for (let i = 0; i < cubeNumbers.length; i++) {
+
+    const cubeResults = cubeNumbers[i] * cubeNumbers[i] * cubeNumbers[i];
+    console.log(cubeResults);
+
+}
+
+
+
+
+
