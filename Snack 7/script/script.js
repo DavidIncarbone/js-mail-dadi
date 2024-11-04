1b666667ccb264483767a256c8d8e58ddd31a43a
DavidIncarbone <david.incarbone@gmail.com>
2024-11-04 20:30:08 +0100

    fixed snack 7

--- Snack 7/script/script.js
+++ Snack 7/script/script.js
@@ -4,11 +4,14 @@ console.clear();
 // Stampa le potenze di 2 fino a 1000.
 
 
+let powResult = 0;
 
-
-for (let p = 1; p <= 10; p++) {
+for (let p = 0; powResult <= 1000; p++) {
     let base = 2;
-    console.log(Math.pow(base, p));
+    powResult = Math.pow(base, p);
+    if (powResult <= 1000) {
+        console.log(powResult);
+    }
 }
 
 
